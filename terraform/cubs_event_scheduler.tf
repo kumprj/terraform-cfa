ed3340b54a748de0a80d227f73e2a8ba6dadb0b3
Robert Kump <rkump24@gmail.com>
2025-04-02 23:17:00 -0500

    revert back

--- terraform/cubs_event_scheduler.tf
+++ terraform/cubs_event_scheduler.tf
@@ -6,7 +6,7 @@ resource "aws_scheduler_schedule" "cubs" {
     mode = "OFF"
   }
 
-  schedule_expression = "cron(16 4 * * ? *)"
+  schedule_expression = "cron(0 1 * * ? *)"
 
   target {
     arn      = "arn:aws:lambda:us-east-1:875660052076:function:cubs-cfa-texter"
